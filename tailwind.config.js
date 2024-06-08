ca1f95c75fe12dada338dd5b96fdc4434932b383
Devansh Parapalli <parapallidev@gmail.com>
2024-06-08 18:35:40 +0530

    setup base vite+tailwindcss+ts

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/*.{js,ts,html}",
+    "./src/**/*.{js,ts,html}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui')
+  ],
+}
+
